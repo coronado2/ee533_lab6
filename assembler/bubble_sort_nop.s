2c355c2a51b9e8246bde59fd7be3237328c1ae9e
Guillermo <gacorona@usc.edu>
2026-02-22 00:13:55 -0800

    bubble_sort nops update

--- assembler/bubble_sort_nop.s
+++ assembler/bubble_sort_nop.s
@@ -2,102 +2,115 @@ ADDI  r7, r0, 512
 NOP
 NOP
 NOP
-
 ADDI  r7, r7, -8
 NOP
 NOP
 NOP
-
 SW    r6, 0(r7)
-
+NOP
+NOP
+NOP
 ADDI  r6, r7, 0
 NOP
 NOP
 NOP
-
 ADDI  r7, r7, -160
 NOP
 NOP
 NOP
-
 ADDI  r4, r6, 0
 NOP
 NOP
 NOP
-
 ADDI  r4, r4, -152
 NOP
 NOP
 NOP
-
-
 ADDI  r1, r0, 323
 NOP
 NOP
 NOP
 SW    r1, 0(r4)
-
+NOP
+NOP
+NOP
 ADDI  r1, r0, 123
 NOP
 NOP
 NOP
 SW    r1, 8(r4)
-
+NOP
+NOP
+NOP
 ADDI  r1, r0, -455
 NOP
 NOP
 NOP
 SW    r1, 16(r4)
-
+NOP
+NOP
+NOP
 ADDI  r1, r0, 2
 NOP
 NOP
 NOP
 SW    r1, 24(r4)
-
+NOP
+NOP
+NOP
 ADDI  r1, r0, 98
 NOP
 NOP
 NOP
 SW    r1, 32(r4)
-
+NOP
+NOP
+NOP
 ADDI  r1, r0, 125
 NOP
 NOP
 NOP
 SW    r1, 40(r4)
-
+NOP
+NOP
+NOP
 ADDI  r1, r0, 10
 NOP
 NOP
 NOP
 SW    r1, 48(r4)
-
+NOP
+NOP
+NOP
 ADDI  r1, r0, 65
 NOP
 NOP
 NOP
 SW    r1, 56(r4)
-
+NOP
+NOP
+NOP
 ADDI  r1, r0, -56
 NOP
 NOP
 NOP
 SW    r1, 64(r4)
-
+NOP
+NOP
+NOP
 ADDI  r1, r0, 0
 NOP
 NOP
 NOP
 SW    r1, 72(r4)
-
+NOP
+NOP
+NOP
 ADDI  r1, r0, 9
 NOP
 NOP
 NOP
 SW    r1, -8(r6)
-
-
 outer_loop:
     LW    r2, -8(r6)
     NOP
@@ -113,13 +126,13 @@ outer_loop:
     NOP
     NOP
     NOP
+    NOP
 
     J     sort_done
     NOP
     NOP
     NOP
-
-
+    NOP
 outer_continue:
     ADDI  r1, r0, 0
     NOP
@@ -144,12 +157,13 @@ inner_loop:
     NOP
     NOP
     NOP
+    NOP
 
     J     inner_done
     NOP
     NOP
     NOP
-
+    NOP
 
 inner_continue:
     ADD   r5, r4, r0
@@ -179,12 +193,13 @@ addr_walk_A:
     NOP
     NOP
     NOP
+    NOP
 
     J     addrA_done
     NOP
     NOP
     NOP
-
+    NOP
 
 do_add8_A:
     ADDI  r5, r5, 8
@@ -202,7 +217,7 @@ do_add8_A:
     NOP
     NOP
     NOP
-
+    NOP
 
 addrA_done:
     LW    r1, 0(r5)
@@ -229,7 +244,7 @@ addrA_done:
     NOP
     NOP
     NOP
-
+    NOP
 
 no_swap:
     SW    r7, -24(r6)
@@ -254,7 +269,7 @@ no_swap:
     NOP
     NOP
     NOP
-
+    NOP
 
 do_swap:
     ADDI  r5, r5, -8
@@ -284,7 +299,7 @@ do_swap:
     NOP
     NOP
     NOP
-
+    NOP
 
 inner_done:
     LW    r2, -8(r6)
@@ -302,7 +317,7 @@ inner_done:
     NOP
     NOP
     NOP
-
+    NOP
 
 sort_done:
     ADDI  r7, r6, 0
@@ -323,6 +338,7 @@ sort_done:
 
 end:
     J     end
+    NOP
     NOP
     NOP
     NOP
--- c_programs/br_test.s
+++ c_programs/br_test.s
@@ -1,49 +1,24 @@
 ADDI r4, r0, 352
-NOP
-NOP
-NOP
-
 ADDI r1, r0, 111
 NOP
 NOP
 NOP
-NOP
-
 SW   r1, 0(r4)
-NOP
-NOP
-NOP
-
 ADDI r2, r0, 1
+ADDI r3, r0, 0
 NOP
 NOP
 NOP
-NOP
-
-ADDI r3, r0, 1
-NOP
-NOP
-NOP
-NOP
-
 BNE  r2, r3, skip_bad
 NOP
 NOP
 NOP
-NOP      
-
+NOP   
 ADDI r1, r0, 222
 NOP
 NOP
 NOP
-NOP
-
 SW   r1, 0(r4)
-NOP
-NOP
-NOP
-NOP
-
 skip_bad:
 end:
 J end
